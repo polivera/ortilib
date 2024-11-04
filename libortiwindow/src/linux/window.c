492121ceb2b1410701a17973be10088ecc6e87fc
polivera <punk.moon2229@fastmail.com>
2024-11-04 02:24:00 +0100

    Open simple window in different OS
    - Simplify makefile.
    - Move all OS specific files to its own folder.

--- libortiwindow/src/linux/window.c
+++ libortiwindow/src/linux/window.c
@@ -1,4 +1,4 @@
-#include "./linux/xdg-shell-protocol.h"
+#include "xdg-shell-protocol.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -31,6 +31,12 @@ struct xdg_wm_base *xdg_shell;
 // This is what we consider the window
 struct xdg_toplevel *xdg_top_level;
 
+/**
+ * INPUTS
+ */
+struct wl_seat *seat;
+struct wl_keyboard *keyboard;
+
 // RGBA pixel
 uint8_t *pixel;
 
@@ -58,6 +64,7 @@ int32_t allocate_shared_memory(uint64_t size) {
  * TODO: Again we have a virtual allocation that might be replace with arena
  */
 void resize_window() {
+  printf("Resize the window! %d\n", color);
   int32_t fd = allocate_shared_memory(window_width * window_heigh * 4);
   pixel = mmap(0, window_width * window_heigh * 4, PROT_READ | PROT_WRITE,
                MAP_SHARED, fd, 0);
@@ -67,7 +74,7 @@ void resize_window() {
   }
   struct wl_shm_pool *pool =
       wl_shm_create_pool(shm, fd, window_width * window_heigh * 4);
-  // window_width * 4 is the stride.
+  // NOTE: Here is the stride
   buffer = wl_shm_pool_create_buffer(pool, 0, window_width, window_heigh,
                                      window_width * 4, WL_SHM_FORMAT_ARGB8888);
   wl_shm_pool_destroy(pool);
@@ -75,48 +82,74 @@ void resize_window() {
 }
 
 void draw() {
+  color++;
   memset(pixel, color, window_width * window_heigh * 4);
+}
 
+void make_frame() {
+  draw();
   wl_surface_attach(surface, buffer, 0, 0);
   wl_surface_damage_buffer(surface, 0, 0, window_width, window_heigh);
   wl_surface_commit(surface);
 }
 
+/**
+ * Callback weird stuff
+ */
 struct wl_callback_listener cb_listener;
-
 void frame_new(void *data, struct wl_callback *cb, uint32_t cb_data) {
   wl_callback_destroy(cb);
   cb = wl_surface_frame(surface);
   wl_callback_add_listener(cb, &cb_listener, 0);
-  color++;
-  draw();
+  make_frame();
 }
 struct wl_callback_listener cb_listener = {.done = frame_new};
 
 void x_surface_config(void *data, struct xdg_surface *x_surface,
                       uint32_t serial) {
   xdg_surface_ack_configure(x_surface, serial);
+  printf("XDG configuration \n");
   if (!pixel) {
     resize_window();
   }
-  draw();
+  make_frame();
 }
 
 struct xdg_surface_listener x_listener = {.configure = x_surface_config};
 
-void top_config(void *data, struct xdg_toplevel *top_level, int32_t width,
-                int32_t height, struct wl_array *states) {}
+void top_config(void *data, struct xdg_toplevel *top_level, int32_t new_width,
+                int32_t new_height, struct wl_array *states) {
+  if (!new_width || !new_height) {
+    return;
+  }
+
+  if (window_width != new_width || window_heigh != new_height) {
+    munmap(pixel, new_width * new_height);
+    window_width = new_width;
+    window_heigh = new_height;
+    resize_window();
+  }
+}
 
 void top_close(void *data, struct xdg_toplevel *top_level) { close_window = 1; }
 
-struct xdg_toplevel_listener top_list = {.configure = top_config,
-                                         .close = top_close};
+struct xdg_toplevel_listener toplevel_listener = {.configure = top_config,
+                                                  .close = top_close};
 
 void shell_ping(void *data, struct xdg_wm_base *sh, uint32_t serial) {
   xdg_wm_base_pong(sh, serial);
 }
 
 struct xdg_wm_base_listener xdg_shell_listener = {.ping = shell_ping};
+
+void seat_capabilities(void *data, struct wl_seat *wl_seat,
+                       uint32_t capabilities) {
+
+  // TODO: Add key listener wl_keyboard_listener.
+}
+void seat_name(void *data, struct wl_seat *wl_seat, const char *name) {}
+struct wl_seat_listener seat_listener = {.capabilities = seat_capabilities,
+                                         .name = seat_name};
 
 /**
  * Global listener function.
@@ -130,6 +163,9 @@ void register_global(void *data, struct wl_registry *registry, uint32_t name,
   } else if (strcmp(interface, xdg_wm_base_interface.name) == 0) {
     xdg_shell = wl_registry_bind(registry, name, &xdg_wm_base_interface, 1);
     xdg_wm_base_add_listener(xdg_shell, &xdg_shell_listener, 0);
+  } else if (strcmp(interface, wl_seat_interface.name) == 0) {
+    seat = wl_registry_bind(registry, name, &wl_seat_interface, 1);
+    wl_seat_add_listener(seat, &seat_listener, 0);
   }
 }
 
@@ -155,7 +191,6 @@ void orti_open_window() {
   struct wl_registry *registry = wl_display_get_registry(display);
   wl_registry_add_listener(registry, &registry_listener, 0);
   // Sync client and server. It sends all pending request to the compositor
-  // (maybe)
   wl_display_roundtrip(display);
 
   surface = wl_compositor_create_surface(compositor);
@@ -167,7 +202,7 @@ void orti_open_window() {
   xdg_surface_add_listener(x_surface, &x_listener, 0);
 
   xdg_top_level = xdg_surface_get_toplevel(x_surface);
-  xdg_toplevel_add_listener(xdg_top_level, &top_list, 0);
+  xdg_toplevel_add_listener(xdg_top_level, &toplevel_listener, 0);
   xdg_toplevel_set_title(xdg_top_level, "some weird title");
   wl_surface_commit(surface);
 
@@ -176,6 +211,7 @@ void orti_open_window() {
       break;
   }
 
+  wl_seat_release(seat);
   if (buffer) {
     wl_buffer_destroy(buffer);
   }
